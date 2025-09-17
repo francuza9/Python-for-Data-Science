c946e834898ec5fa98edf6acca932156db9550a2
francu <untiflash@gmail.com>
2025-09-17 18:44:34 +0200

    docstring for ft_filter changed to original one's

--- Day0/ex06/ft_filter.py
+++ Day0/ex06/ft_filter.py
@@ -1,6 +1,6 @@
 
 def ft_filter(function_to_apply, iterable):
-    """Filters elements from an iterable based on a predicate function."""
+    """filter(function or None, iterable) --> filter object"""
     if function_to_apply is None:
         return [item for item in iterable if item]
     return [item for item in iterable if function_to_apply(item)]
