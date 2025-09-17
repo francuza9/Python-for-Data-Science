cefa2df12e5c1a631d93eca06dfdf586fd20ea51
francu <untiflash@gmail.com>
2025-09-17 18:42:18 +0200

    norminette

--- Day0/ex06/filterstring.py
+++ Day0/ex06/filterstring.py
@@ -3,40 +3,44 @@ import sys
 
 
 def noSpecialChars(s: str) -> bool:
-	""" Checks if the string contains only alphanumeric characters and spaces. """
-	for char in s:
-		if not char.isalnum() and char != ' ':
-			return False
-	return True
+    """
+    Checks if the string contains only
+    alphanumeric characters and spaces.
+    """
+    for char in s:
+        if not char.isalnum() and char != ' ':
+            return False
+    return True
 
 
 def main():
-	try:
+    """
+    Main function to filter words longer \
+    than N characters from the input string S.
+    """
+    try:
+        argv = sys.argv
+        if len(argv) != 3:
+            raise AssertionError("the arguments are bad")
+        S = argv[1]
+        N = argv[2]
+        if not N.lstrip('-').isdigit():
+            raise AssertionError("the arguments are bad")
+        if not noSpecialChars(S):
+            raise AssertionError("the arguments are bad")
+        try:
+            N = int(N)
+        except ValueError:
+            print("AssertionError: the arguments are bad")
+            return (1)
+        words = ft_filter(lambda x: len(x) > N, S.split(' '))
+        print(words)
 
-		argv = sys.argv
-		if len(argv) != 3:
-			raise AssertionError("the arguments are bad")
-		S = argv[1]
-		N = argv[2]
-		if not N.lstrip('-').isdigit():
-			raise AssertionError("the arguments are bad")
-		if not noSpecialChars(S):
-			raise AssertionError("the arguments are bad")
-		try:
-			N = int(N)
-		except ValueError:
-			print("AssertionError: the arguments are bad")
-			return(1)
-		words = ft_filter(lambda x: len(x) > N, S.split(' '))
-		print(words)
-
-	except Exception as e:
-		print("AssertionError:", e)
-		return(1)
-	return(0)
-	
-
+    except Exception as e:
+        print("AssertionError:", e)
+        return (1)
+    return (0)
 
 
 if __name__ == "__main__":
-	main()
+    main()
